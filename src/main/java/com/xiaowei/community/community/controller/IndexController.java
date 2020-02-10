2055907f0fed483a054779267a75a99f94edd7f1
squallandck <lionmagnus@126.com>
2020-02-10 17:47:57 +0800

    add index and bootstrap

---
+++ src/main/java/com/xiaowei/community/community/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.xiaowei.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+
+//    @GetMapping("/index")
+//    public String index(@RequestParam(name = "name") String name, Model model) {
+//        model.addAttribute("name", name);
+//        return "index";
+//    }
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+
+}
